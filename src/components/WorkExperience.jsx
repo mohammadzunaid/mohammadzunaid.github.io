ecdf18b4cee43ebf6bd88ac3f15b009d822d6b5b
Mohammad Zunaid <junaidmohammad53@gmail.com>
2025-03-08 02:26:30 +0530

    Adding Work experience

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -16,13 +16,13 @@ function WorkExperience() {
             <li>
               <div data-aos="fade-left">
                 <span className="gold card-title">
-                  Software Engineer II
+                  Engineer II
                 </span>
                 <div>
-                  <small className="gold col-12 col-md-6 pl-0">
+                  <span className="gold col-12 col-md-6 pl-0">
                     <i className="fa fa-building mr-1" aria-hidden="true"></i>
                     Deloitte
-                  </small>
+                  </span>
                   <small className="gold col-12 col-md-3 float-md-right">
                     <i className="fa fa-calendar mr-1" aria-hidden="true"></i>
                     September 2022 - Present
@@ -31,32 +31,36 @@ function WorkExperience() {
                 <div className="text-white text-justify container">
                   <ul>
                     <li>
-                      Design and develop software solutions by gathering
-                      requirements from the client
+                      Designed and optimized database entities and relationships in PostgreSQL.
                     </li>
                     <li>
-                      Actively participate in planning, tracking and manage
-                      deliverables in Agile environment
+                      Developed dynamic, responsive web pages and network graphs using D3.js and AntV.
                     </li>
                     <li>
-                      Review code and debug errors to improve performance.
+                      Upgraded the Angular application from version 10 to 12.
                     </li>
                     <li>
-                      Write Junits and Integration tests to test the code
-                      written and increase code coverage
+                      Configured RabbitMQ for seamless communication between services.
                     </li>
+                    <li>
+                      Integrated the application with external assets using webhooks and exposed REST APIs.
+                    </li>
+                    <li>
+                       Implemented containerization of services using Docker.
+                    </li>
+                    
                   </ul>
                 </div>
               </div>
             </li>
             <li>
               <div data-aos="fade-left" delay="500">
-                <span className="gold card-title">.NET Full Stack Developer</span>
+                <span className="gold card-title">Software Engineer</span>
                 <div>
-                  <small className="gold col-12 col-md-6 pl-0">
-                    <i className="fa fa-building mr-1" aria-hidden="true"></i>
+                  <span className="gold col-12 col-md-6 pl-0">
+                    <span className="fa fa-building mr-1" aria-hidden="true"></span>
                     Prokarma
-                  </small>
+                  </span>
                   <small className="gold col-12 col-md-3 float-md-right">
                     <i className="fa fa-calendar mr-1" aria-hidden="true"></i>
                     Mar 2021 - August 2022
@@ -64,26 +68,53 @@ function WorkExperience() {
                 </div>
                 <div className="text-white text-justify container">
                   <ul>
-                    <li>Lead offshore team seamlessly</li>
                     <li>
-                      Participated in requirements gathering and design
-                      development meetings
+                      Design and develop software solutions by gathering
+                    requirements from the client
                     </li>
                     <li>
-                      Designed and developed product features using primarily
+                      Actively participate in planning, tracking and manage
+                      deliverables in Agile environment
+                    </li>
+                    <li>
+                      Developed product features using primarily
                       C#, .Net Core, Entity Framework Core, Angular, SQL Server
-                    </li>
-                    <li>
-                      Investigated issues and defects to determine problem root
-                      cause and formulate corrective action recommendations
                     </li>
                     <li>
                       Collaborated in team-based, Agile environment to
                       accomplish objectives by deadlines
                     </li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div data-aos="fade-left" delay="500">
+                <span className="gold card-title">Senior System Engineer</span>
+                <div>
+                  <span className="gold col-12 col-md-6 pl-0">
+                    <span className="fa fa-building mr-1" aria-hidden="true"></span>
+                    Infosys
+                  </span>
+                  <small className="gold col-12 col-md-3 float-md-right">
+                    <i className="fa fa-calendar mr-1" aria-hidden="true"></i>
+                    Dec 2017 - Mar 2021
+                  </small>
+                </div>
+                <div className="text-white text-justify container">
+                  <ul>
                     <li>
-                      Provided KT to testing and production support team after
-                      every release.
+                      Developed user interface components using Angular 8, HTML5, CSS3, and Bootstrap
+                    </li>
+                    <li>
+                      Develop WebApi using .Net Core and Entity Framework Core by following REST guidelines.
+                    </li>
+                    <li>
+                      Investigated issues and defects to determine problem root
+                      cause.
+                    </li>
+                    <li>
+                      Creating testcases using Nunit, Jasmine and Karma.
                     </li>
                   </ul>
                 </div>
